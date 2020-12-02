6217a1749a81741283d1ef5e7edccec563c3113b
Jingying Huang <wphuang@ucdavis.edu>
2020-12-02 08:53:13 -0800

    generate all sequences of N binary bits

--- decode_ways.c
+++ decode_ways.c
@@ -3,17 +3,46 @@
 #include <stdlib.h>
 #include <string.h>
 
-void printSubsequences(int* arr, size_t size) {
-  unsigned int num_subsets = pow(2, size);
-  for (size_t i = 1; i < num_subsets; i++) {
-    for (size_t j = 0; j < size; j++) {
-      if (i & (1 << j)) {
-        printf("%d", arr[j]);
-      }
+/*
+Given a non-empty string containing only digits, determine the total number of
+ways to decode it.
+E.g.
+  Input: s = "12"
+  Output: 2
+  Explanation: It could be decoded as "AB" (1 2) or "L" (12).
+
+  Input: s = "226"
+  Output: 3
+  Explanation: It could be decoded as "BZ" (2 26), "VF" (22 6), or "BBF" (2 2
+  6).
+
+  Mathematically speaking, the number of possible decodings is 2^(n-1)
+  As 12345 would be 0000, 12-345 would be 0100
+*/
+
+void get2dArr(int* arr, int num_subsets, int*** comb, int* track, size_t size) {
+  if (*track < num_subsets) {
+    for (size_t i = 0; i < size; i++) {
+      (*comb)[*track][i] = arr[i];
     }
-    printf("\n");
   }
+  ++*track;
 }
+
+void binaryGen(size_t size, int* bin, size_t i, int*** comb, int* track,
+               int num_subsets) {
+  if (i == size) {
+    get2dArr(bin, num_subsets, comb, track, size);
+    return;
+  }
+  bin[i] = 0;
+  binaryGen(size, bin, i + 1, comb, track, num_subsets);
+
+  bin[i] = 1;
+  binaryGen(size, bin, i + 1, comb, track, num_subsets);
+}
+
+void init() {}
 
 int* convert2Int(char* s, size_t size) {
   size_t i = 0;
@@ -25,13 +54,37 @@ int* convert2Int(char* s, size_t size) {
 }
 
 void numDecodings(char* s) {
+  // Initialization
+  int track = 0;
   size_t size = strlen(s);
   int* arr = convert2Int(s, size);
-  printSubsequences(arr, size);
-  free(arr);
+  int* bin = malloc((size - 1) * sizeof(int));
+  unsigned int num_subsets = pow(2, size - 1);
+  int** comb = malloc(num_subsets * sizeof(bin));
+
+  for (size_t i = 0; i < num_subsets; i++) {
+    comb[i] = malloc((size - 1) * sizeof(int));
+  }
+
+  binaryGen(size - 1, bin, 0, &comb, &track, num_subsets);
+
+  for (size_t i = 0; i < num_subsets; i++) {
+    for (size_t j = 0; j < size - 1; j++) {
+      printf("%d ", comb[i][j]);
+    }
+    printf("\n");
+  }
+
+  if (arr && comb) {
+    free(arr);
+    for (size_t i = 0; i < num_subsets; i++) {
+      free(comb[i]);
+    }
+    free(comb);
+  }
 }
 
 int main() {
-  numDecodings("123");
+  numDecodings("226");
   return 0;
 }
