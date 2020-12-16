57829cf7d035b18eb732b9a7ac495ef33202c87a
Jingying-Huang <wphuang@ucdavis.edu>
2020-12-15 21:34:03 -0800

    Finished it! Next step is to design unit testing

--- valid_number.c
+++ valid_number.c
@@ -1,3 +1,4 @@
+#include <math.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,37 +8,45 @@ bool isNotValid(char *l, char *r) { return strlen(l) == strlen(r); }
 
 bool isE(char *p) { return *p == 'e'; };
 
-bool isInt(char *x) { return atoi(x) != 0; }
+bool isInt(char *x) {
+  int i, r, n;
+  r = sscanf(x, "%d%n", &i, &n);
+  if (r == 1 && n == strlen(x)) {
+    return true;
+  }
+  return false;
+}
+
+bool isFloat(char *x) { return atof(x) != 0.0; }
 
 bool isNumber(char *s) {
   char *p;
   int numl = strtol(s, &p, 10);
 
   char *x;
-  ++p;
   int numr = strtol(p, &x, 10);
+
+  if (isInt(s) || isFloat(x)) {
+    return true;
+  }
 
   if (isNotValid(s, p)) {
     return false;
-  } else if (isE(p)) {
-    if (isNotValid(p, x)) {
-      return false;
-    }
-    ++p;
-
-    if (numr == 0 && isNotValid(p, x)) {
-      return false;
-    } else if (isInt(p)) {
-      return false;
-    }
-    return true;
   }
-  return true;
+
+  if (isE(p)) {
+    if (isNotValid(p, x)) {
+      if (isInt(++x)) {
+        return true;
+      }
+      return false;
+    }
+  }
+  return false;
 }
 
 int main(int argc, char **argv) {
-  char *s = "-1e123a";
+  char *s = "95a54e53";
   bool foo = isNumber(s);
-
   printf("Input is %d\n", foo);
 }
